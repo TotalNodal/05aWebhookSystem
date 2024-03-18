d604c83dbe85cc74382a53c3734add0bdfe1510d
TotalNodal <150134390+TotalNodal@users.noreply.github.com>
2024-03-18 08:56:06 +0100

    program cs, fixed config updated index.html values for webhooktype

--- AirlineSendAgent/Program.cs
+++ AirlineSendAgent/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using System.IO;
 using AirlineSendAgent.App;
+using AirlineSendAgent.Client;
 using AirlineSendAgent.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using AirlineSendAgent.Client;
-using System.IO; 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AirlineSendAgent
 {
@@ -14,29 +14,27 @@ namespace AirlineSendAgent
     {
         static void Main(string[] args)
         {
+
             var builder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddUserSecrets(typeof(Program).Assembly, optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets(typeof(Program).Assembly, optional: true)
+                .AddEnvironmentVariables()
                 .AddCommandLine(args);
 
-            var configuration = builder.Build();
+            var config = builder.Build();
 
-            var host = Host.CreateDefaultBuilder(args)
-                .ConfigureServices((context, services) =>
-                {
-                    //services.AddHostedService<FlightDetailService>();
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) => {
                     services.AddSingleton<IAppHost, AppHost>();
-                    services.AddDbContext<SendAgentDbContext>(options =>
-                    {
-                        //options.UseSqlServer("Server=Desktop-TITA3TC\\VE_SERVER;Database=AirlineWebDB;User Id=sa;Password=Password123!;Trusted_Connection=true;TrustServerCertificate=true;MultipleActiveResultSets=true");
-                        options.UseSqlServer(context.Configuration.GetConnectionString("AirlineConnection"));
-                    });
+                    services.AddSingleton<IWebhookClient, WebhookClient>();
+                    services.AddDbContext<SendAgentDbContext>(opt => opt.UseSqlServer(context.Configuration.GetConnectionString("AirlineConnection")));
+                    //(config.GetConnectionString("AirlineConnection")));
+
+
 
                     services.AddHttpClient();
-                })
-                .Build();
+                }).Build();
 
             host.Services.GetService<IAppHost>().Run();
         }
